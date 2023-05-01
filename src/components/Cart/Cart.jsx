f1229ec0830cc53b5d5437d1174a48c36524eb19
Sarzil727945 <sarzilmuntaha.gmail.com>
2023-05-02 02:27:37 +0600

    cart complete

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Button, Card } from 'react-bootstrap';
+import './Cart.css'
+import { Link } from 'react-router-dom';
+
+const Cart = ({ data }) => {
+     const { picture, name, Experiences, likes, btn, recipesNumber } = data;
+     console.log(data);
+     return (
+          <div className='col-lg-6 mb-3'>
+               <div>
+                    <Card className=' '>
+                         <Card.Body>
+                              <img className=' w-100 rounded imageStyle' src={picture} alt="" />
+                              <h2 className='mt-4'>Name: {name}</h2>
+                              <h4>Numbers of recipes: {recipesNumber}</h4>
+                              <Card.Title >Experience: {Experiences}</Card.Title>
+                              <Card.Subtitle className=" text-muted mb-2">
+
+                              </Card.Subtitle>
+
+                              <Card.Subtitle className=" text-muted my-3">
+
+                                   <span> Likes: {likes} </span>
+                              </Card.Subtitle>
+                              <div className=' text-center'>
+                                   <Button variant="info">
+                                        <Link className='text-decoration-none' to={`/viewRecipes/${recipesNumber}`}>View Recipes </Link>
+                                   </Button>
+                              </div>
+                         </Card.Body>
+                    </Card>
+               </div>
+          </div>
+     );
+};
+
+export default Cart;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,30 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Home.css'
 import Marquee from "react-fast-marquee";
+import Cart from '../Cart/Cart';
 
 const Home = () => {
+     const [allData, setAllData] = useState([])
+
+     useEffect(() => {
+          fetch('http://localhost:5000/chefData')
+               .then(res => res.json())
+               .then(data => setAllData(data))
+     }, [])
+
      return (
           <div className='mt-2 pt-5'>
-               {/* section1 html part starts  */}
 
+               {/* section1  part starts  */}
                <section className="container-fluid section1 pt-5">
                     <Marquee>
                          <h1 className=' text-info'>This is Home Page</h1>
                     </Marquee>
                     <div className='mt-lg-5 pt-lg-5'>
-                         <p className=' fs-1 text-muted text-center mt-lg-5'>Lemon & Limes – Personal Food Blog WordPress Theme</p>
+                         <p className=' fs-1 text-muted text-center mt-lg-5'>Lemon & Limes - Personal Food Blog WordPress Theme</p>
                          <h1 className='text-center display-3 '>
                               Specially designed for food bloggers, cooking enthusiasts and small food businesses.
                          </h1>
                          <div className=' text-center mt-5'>
-                              <button type="button" class="btn btn-light me-3">VIEW DEMOS</button>
-                              <button type="button" class="btn btn-dark">BUY NOW</button>
+                              <button type="button" className="btn btn-light me-3">VIEW DEMOS</button>
+                              <button type="button" className="btn btn-dark">BUY NOW</button>
                          </div>
                     </div>
-
                </section>
-               {/* section1 html part ends */}
+               {/* section1  part ends */}
+               
 
+               {/* section2  part starts */}
+               <section className=' mt-lg-5 container'>
+                    <div className='row mt-3'>
+                         {
+                              allData.map(data => <Cart
+                                   key={data.recipesNumber}
+                                   data={data}
+                              ></Cart>)
+                         }
+                    </div>
+               </section>
+               {/* section2  part ends */}
           </div>
      );
 };
