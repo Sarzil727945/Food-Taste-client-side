9e8573922df3ae12d633cb3b5304fe962da37f93
Sarzil727945 <sarzilmuntaha.gmail.com>
2023-05-04 10:31:00 +0600

    hide firebase config

--- src/firebaseConfig/firebaseConfig.js
+++ src/firebaseConfig/firebaseConfig.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAo5wORypuzZDws-bnHj_YFaUNpPuh7Fc4",
-  authDomain: "assignment10-foodrecipe.firebaseapp.com",
-  projectId: "assignment10-foodrecipe",
-  storageBucket: "assignment10-foodrecipe.appspot.com",
-  messagingSenderId: "580770008390",
-  appId: "1:580770008390:web:01b6ec4379c79cc1e26947"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
